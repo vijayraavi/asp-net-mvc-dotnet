596fd69eb34d2485a50f8440eac1cf4c58ecde2a
Alok Agrawal <alokagarwal111@gmail.com>
2017-12-14 19:40:07 +0530

    adding tests

---
+++ AppUnitTestProject/AppUnitTestProject/AppUnitTestProject1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppUnitTestProject
+{
+    [TestClass]
+    public class AppUnitTestProject1
+    {
+        [TestMethod]
+        [Priority(1)]
+        public void TestMethod1()
+        {
+            System.Console.WriteLine("Test Method 1 Passed");
+            Assert.IsTrue(1 == 1);
+        }
+
+        [TestMethod]
+        [TestCategory("MathTest")]
+        public void TestMethod2()
+        {
+            System.Console.WriteLine("Test Method 2 Failed");
+            Assert.IsTrue(1 == 2);
+        }
+    }
+}
---
+++ AppUnitTestProject/AppUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8008995d-7f9c-46ae-96a5-a86bee0ce262")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
